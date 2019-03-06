1b990877426a9fb02bb66de4c6a9c541d995307b
saitoeku3 <saitoeku3@gmail.com>
2019-03-06 21:53:19 +0900

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,21 @@ module.exports = {
     title: 'KIT Developers Meetup',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, user-scalable==no'
+      },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }]
+  },
+
+  manifest: {
+    lang: 'ja',
+    name: 'KIT Developers Meetup',
+    short_name: 'KIT DM',
+    description: pkg.description,
+    background_color: '#000000'
   },
 
   /*
@@ -56,6 +67,10 @@ module.exports = {
           loader: 'eslint-loader',
           exclude: /(node_modules)/
         })
+      }
+
+      if (!ctx.isDev) {
+        config.output.publicPath = '_nuxt/'
       }
     }
   }
